f1b47903de975f091daa48b0a9416202a8646b50
Lizzy Lu <s103062588@m103.nthu.edu.tw>
2014-12-09 20:31:15 +0800

    Update alignLabel.m

--- Scene Parsing/code/myParsing_rcnn_testSiftFlowDataset_tryNoRGBhist_OK/alignLabel.m
+++ Scene Parsing/code/myParsing_rcnn_testSiftFlowDataset_tryNoRGBhist_OK/alignLabel.m
@@ -1,20 +1,43 @@
 function [ resultL ] = alignLabel( resultL, path, qImgName )
     
-    img=imread(fullfile(path.imagePath,[qImgName '.jpg']));
-    I_gray=rgb2gray(img);
-    %smooth the image by coherence filter:
-    filted_I = CoherenceFilter(I_gray,struct('T',5,'rho',2,'Scheme','I', 'sigma', 1));
-    %adjacent neighborhood  model:
+% INPUT
+% resultL:  H by W matrix, labels of all pixels in query image
+% path:     structure, path for different data,
+%           with fields 'HOMECODE', 'MainFolder', 'imagePath', 'labelImgPath', 'spPath'
+% qImgName: string, query image name
+%
+% OUTPUT
+% resultL:  H by W matrix, smoothed labels of all pixels in query image
+
+    % load query image
+    img = imread(fullfile(path.imagePath, [qImgName '.jpg']));
+    
+    % change color channel to gray image
+    I_gray = rgb2gray(img);
+    
+    % smooth the image by coherence filter
+    filted_I = CoherenceFilter(I_gray, struct('T', 5, 'rho', 2, 'Scheme', 'I', 'sigma', 1));
+    
+    % adjacent neighborhood  model
     seg = graphSeg(filted_I, 0.5, 200, 2, 0);
-    %k-nearest neighborhood model:
+    
+    % k-nearest neighborhood model
     %Lnn = graphSeg(filted_I, 0.5/sqrt(3), 50, 10, 1);
     
-    %figure,imshow(label2rgb(seg));
+    %figure, imshow(label2rgb(seg));
+    
+    % num if segments (max index of segments)
     segNum = max(max(seg));
     
-    for i=1:segNum
-        p=find(seg==i);
-        resultL(p) = mode(resultL(p));
+    for i = 1:segNum % for each segment
+        
+        % get index of pixel belongs to the segment
+        p = find(seg == i);
+        
+        % assign label of pixel which belongs to the segment as the most frequent label in the segment
+        if (~isempty(p))
+            resultL(p) = mode(resultL(p));
+        end
     end
 
 end
